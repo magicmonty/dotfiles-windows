a6934f9aef82bde3456ba5f8a0c0daae8cde4e5d
Martin Gondermann <mgondermann@np4.net>
2021-12-15 13:18:59 +0100

    Fix trouble.nvim warning

--- neovim/after/plugin/trouble.lua
+++ neovim/after/plugin/trouble.lua
@@ -4,7 +4,7 @@ if not status then return end
 local map = require("vim_ext").map
 
 trouble.setup({
-  use_lsp_diagnostic_signs = true
+  use_diagnostic_signs = true
 })
 
 map('n', '<leader>xx', ':TroubleToggle<cr>', {silent = true, noremap = true})
