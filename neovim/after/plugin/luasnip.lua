0daf72bc52f8b37bb866b53f4ecbff1d473cee27
Martin Gondermann <mgondermann@np4.net>
2021-12-15 13:12:13 +0100

    Add Angular snippets

--- neovim/after/plugin/luasnip.lua
+++ neovim/after/plugin/luasnip.lua
@@ -2,3 +2,4 @@ local status, _ = pcall(require, "luasnip")
 if not status then return end
 
 require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({ paths = '~/.dotfiles/snippets'})
